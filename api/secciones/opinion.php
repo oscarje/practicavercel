75c717f9c6add807dae0b100146f82b285dce24d
oscarje <53665647+oscarje@users.noreply.github.com>
2024-11-04 10:30:59 -0500

    Update opinion.php

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Por qué la Nube es el Futuro",
+    "autor" => "Sofía Morales",
+    "resumen" => "Reflexiones sobre cómo la adopción de aplicaciones en la nube está cambiando nuestras vidas y qué esperar en el futuro.",
 ];
 ?>
