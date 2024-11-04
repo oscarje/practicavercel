b358a6c9b304aef43472f56d80baf7dd293cfc4a
oscarje <53665647+oscarje@users.noreply.github.com>
2024-11-04 10:25:16 -0500

    Update empleo.php

--- api/secciones/empleo.php
+++ api/secciones/empleo.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "El Futuro del Trabajo en la Era Digital",
+    "autor" => "Sofía Romero",
+    "resumen" => "Con el avance de la tecnología, el mercado laboral está experimentando transformaciones significativas en todos los sectores.",
 ];
 ?>
