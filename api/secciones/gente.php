a41009ec1f5f91c93623da0d4995dd1bdaef2637
oscarje <53665647+oscarje@users.noreply.github.com>
2024-11-04 10:25:47 -0500

    Update gente.php

--- api/secciones/gente.php
+++ api/secciones/gente.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Historias Inspiradoras de Superación",
+    "autor" => "Mariana Silva",
+    "resumen" => "Personas alrededor del mundo comparten sus historias de superación y éxito personal en tiempos difíciles.",
 ];
 ?>
