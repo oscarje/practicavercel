a3a192f5e6dcef5910272b16cb5ce5ca11cdfe28
oscarje <53665647+oscarje@users.noreply.github.com>
2024-11-04 10:31:26 -0500

    Update sociedad.php

--- api/secciones/sociedad.php
+++ api/secciones/sociedad.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $sociedad = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "La Nube en la Vida Cotidiana",
+    "autor" => "José Martínez",
+    "resumen" => "Desde el trabajo remoto hasta la educación, las aplicaciones en la nube están cada vez más presentes en la vida diaria.",
 ];
 ?>
