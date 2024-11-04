739e4c0d414792a3a75ecfc41f2c83990800b5e7
oscarje <53665647+oscarje@users.noreply.github.com>
2024-11-04 10:25:58 -0500

    Update git.php

--- api/secciones/git.php
+++ api/secciones/git.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $git = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Git: La Herramienta Esencial para Desarrolladores",
+    "autor" => "Ricardo Sánchez",
+    "resumen" => "Git se ha convertido en una herramienta indispensable para el desarrollo colaborativo en proyectos de software.",
 ];
 ?>
