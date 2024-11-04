487d354f8d716166a75d57719e99ffed10ca14f9
oscarje <53665647+oscarje@users.noreply.github.com>
2024-11-04 10:25:05 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Perspectivas Económicas para el 2024",
+    "autor" => "Ana López",
+    "resumen" => "Expertos en economía comparten sus previsiones para el próximo año, destacando los sectores con mayor potencial de crecimiento.",
 ];
 ?>
