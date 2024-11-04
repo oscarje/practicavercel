6603ad473f3d8ca1ed9c763180ec14ca25b1c4c3
oscarje <53665647+oscarje@users.noreply.github.com>
2024-11-04 10:26:16 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Crisis Globales y su Impacto en la Política Internacional",
+    "autor" => "Luis Gómez",
+    "resumen" => "Conflictos y tensiones en diversas regiones están moldeando el panorama geopolítico mundial.",
 ];
 ?>
