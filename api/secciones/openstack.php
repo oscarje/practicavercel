19d4dc6fe86ac103724b7f7929f376443c08630a
oscarje <53665647+oscarje@users.noreply.github.com>
2024-11-04 10:30:44 -0500

    Update openstack.php

--- api/secciones/openstack.php
+++ api/secciones/openstack.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openstack = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "OpenStack: La Plataforma Abierta para la Nube",
+    "autor" => "Javier Ortega",
+    "resumen" => "OpenStack es una solución de nube de código abierto que permite a las empresas tener más control sobre su infraestructura.",
 ];
 ?>
