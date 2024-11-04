02f32c433a1520309136ad95e80b5d0fc373fe86
oscarje <53665647+oscarje@users.noreply.github.com>
2024-11-04 10:22:40 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Avances en la Exploración Espacial",
+"autor" => "Dr. Ana López",
+"resumen" => "La NASA ha lanzado una nueva misión para estudiar la superficie de Marte y buscar signos de vida pasada en el planeta rojo",
 ];
 ?>
