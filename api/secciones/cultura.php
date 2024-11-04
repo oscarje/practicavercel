2e387d82386ce45856fee1066e983f43518aa43b
oscarje <53665647+oscarje@users.noreply.github.com>
2024-11-04 10:24:33 -0500

    Update cultura.php

--- api/secciones/cultura.php
+++ api/secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Revitalización de las Lenguas Indígenas",
+    "autor" => "Laura García",
+    "resumen" => "Iniciativas en América Latina buscan preservar y revitalizar las lenguas indígenas en peligro de extinción.",
 ];
 ?>
