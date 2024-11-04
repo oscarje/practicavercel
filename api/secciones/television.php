ee8ca23e3aa0784591e844c7c57e937732d55c6a
oscarje <53665647+oscarje@users.noreply.github.com>
2024-11-04 10:31:54 -0500

    Update television.php

--- api/secciones/television.php
+++ api/secciones/television.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $television = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Televisión en la Nube: La Nueva Era del Streaming",
+    "autor" => "Andrés Navarro",
+    "resumen" => "La nube ha transformado el mundo de la televisión y el entretenimiento, permitiendo contenido bajo demanda desde cualquier lugar.",
 ];
 ?>
