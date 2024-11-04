e48bd53b35af004288c81725db12bac1c7a6bdfe
oscarje <53665647+oscarje@users.noreply.github.com>
2024-11-04 10:27:43 -0500

    Update contenedores.php

--- api/secciones/contenedores.php
+++ api/secciones/contenedores.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $contenedores = [
-"titulo" => "Tecnología de Contenedores para el Desarrollo de Aplicaciones",
-"autor" => "Miguel Torres",
-"resumen" => "El uso de contenedores está revolucionando la industria del software, permitiendo un desarrollo y despliegue más ágil",
+    "titulo" => "Contenedores en la Nube: La Base del Desarrollo Moderno",
+    "autor" => "Miguel Torres",
+    "resumen" => "El uso de contenedores permite la portabilidad y escalabilidad de aplicaciones en la nube, facilitando el trabajo de desarrolladores.",
 ];
 ?>
