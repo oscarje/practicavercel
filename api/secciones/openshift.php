978f8b553e775b711f8226ab22d3b1e8ef4dd53a
oscarje <53665647+oscarje@users.noreply.github.com>
2024-11-04 10:30:25 -0500

    Update openshift.php

--- api/secciones/openshift.php
+++ api/secciones/openshift.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openshift = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "OpenShift: Potenciando la Nube Empresarial",
+    "autor" => "Fernando Vega",
+    "resumen" => "OpenShift facilita la implementación y gestión de aplicaciones en la nube, ofreciendo soluciones empresariales robustas.",
 ];
 ?>
