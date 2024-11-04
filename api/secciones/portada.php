b2ebf5fd2a8efa48b242713489e0422d9b223bda
oscarje <53665647+oscarje@users.noreply.github.com>
2024-11-04 10:31:14 -0500

    Update portada.php

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "Tendencias",
-"autor" => "sistemas@uns",
-"resumen" => "Las ultimas tendencias 2024",
+    "titulo" => "Tendencias en Aplicaciones en la Nube para 2024",
+    "autor" => "Redacción",
+    "resumen" => "Un vistazo a las principales tendencias que marcarán el futuro de las aplicaciones en la nube este año.",
 ];
 ?>
