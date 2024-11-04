17d69ed6f26d52d472f4774347d4939786b80c82
oscarje <53665647+oscarje@users.noreply.github.com>
2024-11-04 10:23:52 -0500

    Update contenedores.php

--- api/secciones/contenedores.php
+++ api/secciones/contenedores.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Tecnología de Contenedores para el Desarrollo de Aplicaciones",
+"autor" => "Miguel Torres",
+"resumen" => "El uso de contenedores está revolucionando la industria del software, permitiendo un desarrollo y despliegue más ágil",
 ];
 ?>
