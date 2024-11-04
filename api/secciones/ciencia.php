1579ee4ee9f2325ccf8ae2a9a35224e79c91615a
oscarje <53665647+oscarje@users.noreply.github.com>
2024-11-04 10:27:28 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "Avances en la Exploración Espacial",
-"autor" => "Dr. Ana López",
-"resumen" => "La NASA ha lanzado una nueva misión para estudiar la superficie de Marte y buscar signos de vida pasada en el planeta rojo",
+    "titulo" => "La Ciencia Detrás de la Nube",
+    "autor" => "Dr. Elena Martínez",
+    "resumen" => "Exploramos cómo los avances en la ciencia están permitiendo el desarrollo de aplicaciones en la nube más rápidas y seguras.",
 ];
 ?>
