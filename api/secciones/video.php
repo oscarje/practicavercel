ef3f92f99b72fec4bd77dbf380a1b57edce60b94
oscarje <53665647+oscarje@users.noreply.github.com>
2024-11-04 10:32:08 -0500

    Update video.php

--- api/secciones/video.php
+++ api/secciones/video.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $video = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Video en la Nube: Almacenamiento y Edición en Línea",
+    "autor" => "Patricia Ramírez",
+    "resumen" => "Las aplicaciones de video en la nube permiten almacenar, editar y compartir contenido de manera ágil y eficiente.",
 ];
 ?>
