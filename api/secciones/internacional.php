12a25e6383aa12c154981017591179bd16211e50
oscarje <53665647+oscarje@users.noreply.github.com>
2024-11-04 10:29:54 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-    "titulo" => "Crisis Globales y su Impacto en la Política Internacional",
+    "titulo" => "La Nube en el Escenario Internacional",
     "autor" => "Luis Gómez",
-    "resumen" => "Conflictos y tensiones en diversas regiones están moldeando el panorama geopolítico mundial.",
+    "resumen" => "Exploramos cómo diferentes países están adoptando la nube para fomentar la innovación y mejorar la competitividad.",
 ];
 ?>
