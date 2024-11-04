fb3f272eb2381370d4e892ac881eb8d6cda4d58c
oscarje <53665647+oscarje@users.noreply.github.com>
2024-11-04 10:27:07 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Reformas Sociales en el País",
+    "autor" => "Carla Fernández",
+    "resumen" => "El gobierno anuncia nuevas reformas sociales para mejorar la calidad de vida de los ciudadanos.",
 ];
 ?>
