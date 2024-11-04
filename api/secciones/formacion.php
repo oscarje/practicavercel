c1e822452786e7a9166d54025380489697bd2e94
oscarje <53665647+oscarje@users.noreply.github.com>
2024-11-04 10:25:34 -0500

    Update formacion.php

--- api/secciones/formacion.php
+++ api/secciones/formacion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $formacion = [
-"titulo" => "XXX",
-"autor" => "sistemas@uns",
-"resumen" => "XXX",
+    "titulo" => "Nuevos Métodos de Aprendizaje Online",
+    "autor" => "Daniel Pérez",
+    "resumen" => "Las plataformas de aprendizaje en línea están cambiando la educación, ofreciendo cursos accesibles a nivel global.",
 ];
 ?>
